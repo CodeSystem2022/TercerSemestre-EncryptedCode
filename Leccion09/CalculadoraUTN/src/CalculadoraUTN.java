109a92a599c4589eb188f49177f4380efd56a8cb
Andrea Llavel <andryllavel20@gmail.com>
2023-06-08 03:10:41 -0300

    Recuperando aplicacion

---
+++ Leccion09/CalculadoraUTN/src/CalculadoraUTN.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class CalculadoraUTN {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+          //Fin while
+    }   //Fin main
+} //Fin clase
