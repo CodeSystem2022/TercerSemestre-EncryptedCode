4a4f382e8c8ac2ccf67cb5437387b88b4022d883
RocioPulitta <109706451+RocioPulitta@users.noreply.github.com>
2023-06-10 16:56:02 -0300

    Update i_registros.py

--- Python/Clase5/i_registros.py
+++ Python/Clase5/i_registros.py
@@ -1,3 +1,4 @@
+#Rocio Pulitta
 import psycopg2  # Esto es para poder conectarnos a Postgre
 
 conexion = psycopg2.connect(user='postgres', password='admin', host='127.0.0.1', port='5432', database='test_bd')
